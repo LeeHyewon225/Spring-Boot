b5093e400c3f27cfb40727fccf6864f57f2d4042
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-06 01:01:47 +0900

    post update, find function and test

--- src/test/java/org/example/springboot/web/PostsApiControllerTest.java
+++ src/test/java/org/example/springboot/web/PostsApiControllerTest.java
@@ -31,6 +31,13 @@ public class PostsApiControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+    /*
+    Rest : HTTP URL 을 통해 자원을 명시하고 HTTP Method 를 통해 해당 자원에 대한 CRUD Operation 을 적용하는 것
+    Rest Api : Rest의 원리를 따르는 Api
+    RestTemplate : 간편하게 Rest Api를 호출할 수 있는 클래스
+    TestRestTemplate : 더 간단하게 통합테스트를 할 수 있는 대체안
+     */
+
 
     @Autowired
     private PostsRepository postsRepository;
@@ -53,9 +60,9 @@ public class PostsApiControllerTest {
         String url = "http://localhost:" + port + "/api/v1/posts";
 
         //사용자의 HTTPRequest에 대한 응답 데이터 포함(HttpStatus, HttpHeaders, HttpBody)
-        //MVC
+        //HttpEntity<T> 를 상속받음                                          해당 url 로 보낼 객체
         ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
-
+                                                           //POST 요청을 보내고 객체로 ResponseEntity 를 받음
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
@@ -81,18 +88,19 @@ public class PostsApiControllerTest {
                 .content(expectedContent)
                 .build();
 
-        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId; //@PathVariable 필드
 
+        //사용자의 HttpRequest에 대한 응답 데이터를 포함하는 클래스(HttpHeader, HttpBody)
+        //TestRestTemplate 의 exchange 메소드를 사용하려면 (해당 url로 보낼 객체를 담은)HttpEntity 객체를 넘겨야 함.
         HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
         ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
-
+                                                           //어떤 HTTP Method 사용 가능   해당 url로 보낼 객체
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
-
     }
 }
 
