96c32c5011172f09284cbabdd67f03827304b17e
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-05 23:43:44 +0900

    post update, find function and test

--- src/main/java/org/example/springboot/service/posts/PostsService.java
+++ src/main/java/org/example/springboot/service/posts/PostsService.java
@@ -1,8 +1,11 @@
 package org.example.springboot.service.posts;
 
 import lombok.RequiredArgsConstructor;
+import org.example.springboot.domain.posts.Posts;
 import org.example.springboot.domain.posts.PostsRepository;
+import org.example.springboot.web.dto.PostsResponseDto;
 import org.example.springboot.web.dto.PostsSaveRequestDto;
+import org.example.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,5 +18,28 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+        /*
+       직접 DB에 쿼리를 날리는 부분 X
+        JPA 의 영속성 컨텍스트(엔티티 영구 저장)
+        트랜잭션 안에서 DB의 데이터를 가져오면 이 데이터는 영속성 컨텍스트가 유지된 상태
+        이 상태에서 데이터를 변경하면 트랜잭션이 끝나는 시점에 해당 테이블에 변경분을 반영
+        즉 엔티티의 값만 변경하면 Updata 쿼리를 날릴 필요 X -> 더티 체킹
+        */
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/org/example/springboot/web/PostsApiController.java
+++ src/main/java/org/example/springboot/web/PostsApiController.java
@@ -2,11 +2,10 @@ package org.example.springboot.web;
 
 import lombok.RequiredArgsConstructor;
 import org.example.springboot.service.posts.PostsService;
+import org.example.springboot.web.dto.PostsResponseDto;
 import org.example.springboot.web.dto.PostsSaveRequestDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor //Bean 주입 받기 : @AutoWierd, setter, *생성자*
 @RestController //스프링에서 서비스를 지정하는 어노테이션
@@ -17,5 +16,16 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
                     //클라이언트가 json 기반의 HTTP Body 로 POST 요청을 할 경우 이를 자바 객체로 변환해서 받음
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+                       //PostMapping 주소의 {변수}와 동일한 이름을 갖는 파라미터 추가
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
